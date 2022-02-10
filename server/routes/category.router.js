5fe78931df60fc4a67f978a95ba6212c98b74af2
Jason Henrickson <jason.henrickson15@gmail.com>
2022-02-10 13:34:42 -0600

    category routers working

--- server/routes/category.router.js
+++ server/routes/category.router.js
@@ -17,4 +17,43 @@ router.get('/', (req, res) => {
     });
 });
 
+router.post('/', (req,res) => {
+  console.log(req.body.name);
+  const category = req.body.name;
+  const queryText = `INSERT INTO "category" ("name") VALUES ($1);`;
+  pool.query(queryText, [category])
+  .then((response) => {
+    console.log('POSTED to categories', response);
+    res.sendStatus(200);
+  }).catch((error) => {
+    console.log('error POSTING to categories', error)
+    res.sendStatus(500);
+  })
+})
+
+router.delete('/:id', (req,res) => {
+  const id = req.params.id;
+  const queryText = `DELETE FROM "category" WHERE id=$1;`;
+  pool.query(queryText, [id]).then((response) => {
+    console.log('DELETED from category', response);
+    res.sendStatus(200);
+  }).catch((error) => {
+    console.log('error DELETING from category', error);
+    res.sendStatus(500);
+  })
+})
+
+router.put('/:id', (req,res) => {
+  const newName = req.body.newName;
+  const id = req.params.id;
+  const queryText = `UPDATE "category" SET "name"=$1 WHERE "id"=$2;`;
+  pool.query(queryText, [newName, id]).then((response) => {
+    console.log('UPDATED category name', response);
+    res.sendStatus(200);
+  }).catch((error) => {
+    console.log('error UPDATING category name', error);
+    res.sendStatus(500);
+  })
+})
+
 module.exports = router;
