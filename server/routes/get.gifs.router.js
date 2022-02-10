565802bed3bf76df91ea48656733cd0d607bce00
Jason Henrickson <jason.henrickson15@gmail.com>
2022-02-10 12:19:57 -0600

    gifs router working

--- server/routes/get.gifs.router.js
+++ server/routes/get.gifs.router.js
@@ -2,19 +2,20 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
-import { useSelector } from 'react-redux';
-const search = useSelector(store => store.searchResults)
 
-router.get('/', (req, res) => {
+router.get('/:search', (req, res) => {
+    console.log('this should be the search',req.params.search)
+    let search = req.params.search
     axios({
         method: 'GET',
-        url: `https://api.giphy.com/v1/gifs/search?q=${search}&api_key=${process.env.GIPHY_API_KEY}&limit=1&rating=r`
+        url: `https://api.giphy.com/v1/gifs/search?q=${search}&api_key=${process.env.GIPHY_API_KEY}&limit=10&rating=r`
     }).then((response) => {
         console.log('response from giphy', response.data);
         const giphyResponse = response.data
         res.send(giphyResponse.data)
     }).catch((error) => {
         console.log('error from giphy', error);
+        res.sendStatus(500);
     })
 })
 
