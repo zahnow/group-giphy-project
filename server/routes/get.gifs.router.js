b5972e110ab286abf5d467c00bb586aaee437ac6
Jason Henrickson <jason.henrickson15@gmail.com>
2022-02-10 11:50:22 -0600

    git gifs url updated to search

--- server/routes/get.gifs.router.js
+++ server/routes/get.gifs.router.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
+import { useSelector } from 'react-redux';
+const search = useSelector(store => store.searchResults)
+
 router.get('/', (req, res) => {
     axios({
         method: 'GET',
-        url: `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.GIPHY_API_KEY}&limit=10`,
+        url: `https://api.giphy.com/v1/gifs/search?q=${search}&api_key=${process.env.GIPHY_API_KEY}&limit=1&rating=r`
     }).then((response) => {
         console.log('response from giphy', response.data);
         const giphyResponse = response.data
