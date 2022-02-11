4a26c45201cfaf594148cb4ef80ee1d6ab08323d
Jason Henrickson <jason.henrickson15@gmail.com>
2022-02-11 13:28:17 -0600

    Moved css to GalleryItem

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,7 +1,8 @@
-
+import './GalleryItem.css';
 import { useDispatch } from 'react-redux';
 
 function GalleryItem(props) {
+    let gif = props.giphy
     console.log('the props coming: ',props.giphy);
     let path = props.giphy?.images?.original?.url;
     let name = props.giphy.id;
@@ -15,10 +16,14 @@ function GalleryItem(props) {
         });
     }
     return (
-        <li>
-            {<img className="gifs" src={props.giphy?.images?.original?.url} />}
+        <>
+        <div className='gifItemContainer'>
+            <img className="gifs" key={gif.id} src={gif?.images?.original?.url} />
+            <h3 className='gifTitle'>{gif.title}</h3>
+            <p className='lineBreak'>this is invisible on purpose for the line break, dont touch</p> {/*dont touch it works*/}
             <button onClick={FavoriteItem}>Heart</button>
-        </li>
+        </div>
+        </>
     );
 }
 
