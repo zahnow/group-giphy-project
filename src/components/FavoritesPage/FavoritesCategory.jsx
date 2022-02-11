7464532b3bdac61e52e42da169e41c339649bfc1
Nick Zahnow <nzahnow@gmail.com>
2022-02-11 11:49:02 -0600

    favorites page and categories sort of working

--- src/components/FavoritesPage/FavoritesCategory.jsx
+++ src/components/FavoritesPage/FavoritesCategory.jsx
@@ -1,21 +1,23 @@
 import {useEffect} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
+import { useParams } from 'react-router-dom';
 import {NavLink} from 'react-router-dom';
-import './Favorites.css';
 
-function FavoritesPage(){
+
+function FavoritesCategory () {
     const dispatch = useDispatch();
     const favorites = useSelector(store => store.favorites);
     const categories = useSelector(store => store.categories);
+    const params = useParams();
 
     useEffect(() => {
         dispatch({type: 'FETCH_CATEGORIES'});
-        dispatch({type: 'FETCH_ALL_FAVORITES'})
+        dispatch({type: 'FETCH_FAVORITES_IN_CATEGORY', payload: params.id});
     }, []);
 
     return(
         <>
-            <h1>Favorites</h1>
+            <h1>{categories.find(category => category?.id === params?.id)} Favorites</h1>
             <div className='category-nav'>
                 <ul>
                     {categories.map(category => (<li key={category.id}> <NavLink to={`/favorites/${category.id}`} > {category.name} </NavLink> </li>))}
@@ -24,7 +26,7 @@ function FavoritesPage(){
 
             <h2>favorites</h2>
             {favorites.map(favorite => {return (
-                <div key={favorite.id}>
+                <div>
                     <h3>{favorite.name}</h3>
                     <img src={favorite.path} />
                 </div>
@@ -32,4 +34,5 @@ function FavoritesPage(){
         </>
     );
 }
-export default FavoritesPage;
+
+export default FavoritesCategory;
